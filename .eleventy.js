98c39a6e477ade8e40313f6b0572e53155796c38
Denperidge <denperidge@gmail.com>
2025-02-07 10:48:25 +0100

    First draft of age calculation

--- .eleventy.js
+++ .eleventy.js
@@ -10,5 +10,13 @@ export const config = {
 export default function(eleventyConfig) {
     eleventyConfig.addDataExtension("yml,yaml", (contents) => yaml.load(contents));
     eleventyConfig.addPassthroughCopy("src/static/");
+
+    // See https://www.11ty.dev/docs/languages/liquid/#multiple-filter-arguments
+    eleventyConfig.addLiquidFilter(
+        "calculateAge", function(birthdateStr, deathdateStr) {
+            const ageInMs = Date.parse(deathdateStr) - Date.parse(birthdateStr);
+            return ageInMs / 31556952000;  // 31.556.952.000 milliseconds in a year
+        }
+    )
 }
 
