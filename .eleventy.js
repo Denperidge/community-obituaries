46e2fa70cb5ae54629e8d65e2c8845100d729954
Denperidge <denperidge@gmail.com>
2025-02-07 12:44:57 +0100

    Fixed date handling

--- .eleventy.js
+++ .eleventy.js
@@ -1,10 +1,4 @@
-import fs from "fs";
-
 import yaml from "js-yaml";
-import dayjs from "dayjs";
-import relativeTime from "dayjs/plugin/relativeTime.js";
-
-dayjs.extend(relativeTime);
 
 export const config = {
     dir: {
@@ -17,9 +11,6 @@ export default function(eleventyConfig) {
     eleventyConfig.addDataExtension("yml,yaml", (contents) => yaml.load(contents));
     eleventyConfig.addPassthroughCopy("src/static/");
 
-
-    const dateFormats = yaml.load(fs.readFileSync(eleventyConfig.directories.data + "config.yaml", {encoding: "utf-8"})).date_formats;
-    console.log(dateFormats)
     // See https://www.11ty.dev/docs/languages/liquid/#multiple-filter-arguments
     eleventyConfig.addLiquidFilter(
         "calculateAge", function(birthdateStr, deathdateStr) {
@@ -28,28 +19,16 @@ export default function(eleventyConfig) {
              * - Can't do 3.9 --> 4.0, that'll make inaccurate calculations
              * It should just not,,, use milliseconds right?
              */
-            const birthdate = dayjs(birthdateStr, dateFormats);
-            const deathdate = dayjs(deathdateStr, dateFormats);
-
-            console.log(birthdateStr)
-            console.log(birthdate)
-
-            return birthdate.from(deathdate, true)
-
-            return
-            /*
             const birthdate = new Date(birthdateStr);
             const deathdate = new Date(deathdateStr);
             let ageInYears = deathdate.getFullYear() - birthdate.getFullYear();
-            if (birthdate.getMonth() >= deathdate.getMonth()) {
-                if (birthdate.getDate() > deathdate.getDate()) {
-
-                }
+            if (
+                birthdate.getMonth() >= deathdate.getMonth() &&
+                birthdate.getDate() > deathdate.getDate()
+            ) {
+                ageInYears -= 1;
             } 
-            console.log(deathdate)
-            return ageInYears
-            */
-            return ageInMs / 31556952000;  // 31.556.952.000 milliseconds in a year
+            return ageInYears;
         }
     )
 }
